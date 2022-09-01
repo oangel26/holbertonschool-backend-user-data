7bfdc22f365464ffdbe309d0c274530ec87def25
oangel26 <oa.angel26@gmail.com>
2022-09-01 17:19:14 -0500

    style fix

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -24,8 +24,8 @@ def filter_datum(fields: List[str], redaction: str, message: str,
 
 
 if __name__ == "__main__":
-    personal_info1 = f'name=egg;email=eggmin@eggsample.com;password=eggcellent;\
-    date_of_birth=12/12/1986;'
+    personal_info1 = f'name=egg;email=eggmin@eggsample.com;\
+    password=eggcellent;date_of_birth=12/12/1986;'
     personal_info2 = """name=bob;email=bob@dylan.com;password=bobbycool;\
     date_of_birth=03/04/1993;"""
 
