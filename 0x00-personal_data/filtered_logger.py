fb976f172413aec5e203ca943abc4b06f0e5013a
oangel26 <oa.angel26@gmail.com>
2022-09-02 09:04:03 -0500

    first commit

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -62,11 +62,3 @@ def get_logger() -> logging.Logger:
     return logger
 
     return logging.Logger(RedactingFormatter.__name__)
-
-
-if __name__ == "__main__":
-    fields = ["password", "date_of_birth"]
-    messages = ["name=egg;email=eggmin@eggsample.com;password=eggcellent;date_of_birth=12/12/1986;", "name=bob;email=bob@dylan.com;password=bobbycool;date_of_birth=03/04/1993;", ""]
-
-    for message in messages:
-        print(filter_datum(fields, 'xxx', message, ';'))
