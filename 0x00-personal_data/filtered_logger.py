7488c5f2c21950e7460c1de56e64e03b474fd43c
oangel26 <oa.angel26@gmail.com>
2022-09-02 09:02:41 -0500

    first commit

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -23,7 +23,7 @@ def filter_datum(fields: List[str], redaction: str, message: str,
     new_message = message
     for field in fields:
         new_message = re.sub(field + "=" + f"[^,{separator}]+",
-                             " " + field + "=" + redaction, new_message)
+                             field + "=" + redaction, new_message)
 
     return new_message
 
@@ -65,5 +65,8 @@ def get_logger() -> logging.Logger:
 
 
 if __name__ == "__main__":
-    get_logger()
-    print("PII_FIELDS: {}".format(len(PII_FIELDS)))
+    fields = ["password", "date_of_birth"]
+    messages = ["name=egg;email=eggmin@eggsample.com;password=eggcellent;date_of_birth=12/12/1986;", "name=bob;email=bob@dylan.com;password=bobbycool;date_of_birth=03/04/1993;", ""]
+
+    for message in messages:
+        print(filter_datum(fields, 'xxx', message, ';'))
